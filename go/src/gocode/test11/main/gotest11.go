45ebbc2eff86bc46941b5aed10d592868bd7bec1
ning_qb <ning_qb@hisuntech.com>
2019-03-12 11:18:47 +0800

    new file:   go/src/gocode/test10/main/gotest10.exe 	new file:   go/src/gocode/test10/main/gotest10.go 	new file:   go/src/gocode/test11/main/gotest11.exe 	new file:   go/src/gocode/test11/main/gotest11.go 	new file:   go/src/gocode/test11/model/utils.go

---
+++ go/src/gocode/test11/main/gotest11.go
@@ -0,0 +1,12 @@
+package main
+import (
+	"fmt"
+	// 为了使用utils.go 文件的变量和函数，我们需要先引用该model
+	"../model"
+)
+
+func main(){
+	// 变量在同一作用域不能重名
+	// 我们使用utils.go 的HeroName 包名.标志符
+	fmt.Println(model.HeroName)
+}
