3a53ba529fe72749bc55983ff7af28f56cb74448
ning_qb <ning_qb@hisuntech.com>
2019-03-12 17:41:24 +0800

    11

--- go/src/gocode/test12/main/gotest12.go
+++ go/src/gocode/test12/main/gotest12.go
@@ -4,6 +4,8 @@ import (
 )
 
 func main(){
+
+	// 方法1
 	var name string
 	var age byte
 	var sal float32
@@ -16,6 +18,11 @@ func main(){
 	fmt.Scanln(&sal)
 	fmt.Println("是否通过考试：")
 	fmt.Scanln(&ispass)
+	fmt.Printf("姓名是 %v，年龄是 %v，薪水是 %v，是否通过考试 %v", name, age, sal, ispass)
+	
+	// 方法2
+	fmt.Println("请输入姓名，年龄，薪水，是否通过考试，使用空格隔开")
+	fmt.Scanf("%s %d %f %t",&name, &age, &sal, &ispass)
 	fmt.Printf("姓名是 %v，年龄是 %v，薪水是 %v，是否通过考试 %v", name, age, sal, ispass)
 
 }
