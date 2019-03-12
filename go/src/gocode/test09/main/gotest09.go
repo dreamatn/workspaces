6720e9c84aa8cbd8fa281a8387d91a33156e4922
ning_qb <ning_qb@hisuntech.com>
2019-03-12 09:24:21 +0800

    1

--- go/src/gocode/test09/main/gotest09.go
+++ go/src/gocode/test09/main/gotest09.go
@@ -1,8 +1,12 @@
 package main
 import (
 	"fmt"
+	"strconv"
 )
 
 func main(){
-	fmt.Println("hello world")
+	var str4 string = "1023"
+	var n3 int64 = 11
+	n3, _ = strconv.ParseInt(str4, 10, 64)
+	fmt.Printf("n3 type %T n3 = %v\n",n3 ,n3)
 }
