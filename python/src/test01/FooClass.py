cc0917c063000371524949f37a8edcb800b6b1e0
ning_qb <ning_qb@hisuntech.com>
2019-03-14 17:48:20 +0800

    u

--- python/src/test01/FooClass.py
+++ python/src/test01/FooClass.py
@@ -1,2 +1,22 @@
-class FooClass():
-    """my"""
+class FooClass(object):
+    """my very first class : FooClass"""
+    version = 0.1 # class data attribute
+
+    def __init__(self, nm='John doe'):
+        """constructor"""
+        self.name = nm
+        print('Created a class instance for ', nm)
+
+    def showName(self):
+        """display instance attritude and class name"""
+        print("Your name is ",self.name)
+        print("My name is ", self.__class__.__name__)
+
+    def showVer(self):
+        """display class static attritude"""
+        print("Your version is ", self.version)
+
+fool = FooClass()
+fool.showName()
+fool.showVer()
+
