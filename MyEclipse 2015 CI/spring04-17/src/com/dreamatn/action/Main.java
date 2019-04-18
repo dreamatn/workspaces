66c56cf8161a5214aefec9692b51b09da775cfb3
ningqb <ning_qb@hisuntech.com>
2019-04-18 09:12:06 +0800

    -a

--- MyEclipse 2015 CI/spring04-17/src/com/dreamatn/action/Main.java
+++ MyEclipse 2015 CI/spring04-17/src/com/dreamatn/action/Main.java
@@ -2,11 +2,14 @@ package com.dreamatn.action;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
 
 import com.dreamatn.dao.Person;
 
+@Service
 public class Main {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// 创建IOC容器
 		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
