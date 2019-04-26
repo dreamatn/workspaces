18a51d4146c84f80b3756172d3c2cedddd781e3b
ningqb <ning_qb@hisuntech.com>
2019-04-26 17:02:47 +0800

    3

---
+++ pyProjcct/mypython/src/test/20190426/domo01/gopherclient.py
@@ -0,0 +1,18 @@
+import socket, sys
+
+port = 70
+host = sys.argv[1]
+filename = sys.argv[2]
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+s.connect((host,port))
+
+# send 接受 bytes
+s.sendall((filename + "\r\n").encode())
+
+while 1:
+    buf = s.recv(2048).decode()
+    if not len(buf):
+        break
+    sys.stdout.write(buf)
