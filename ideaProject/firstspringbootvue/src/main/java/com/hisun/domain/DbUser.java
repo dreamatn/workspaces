ccf1eafebae1da8e9965ac55ae31b72bc782c17d
ningqb <ning_qb@hisuntech.com>
2019-04-26 14:31:01 +0800

    -a

---
+++ ideaProject/firstspringbootvue/src/main/java/com/hisun/domain/DbUser.java
@@ -0,0 +1,18 @@
+package com.hisun.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by MoHuaiyi
+ * 2019/4/23 9:48
+ */
+@Getter
+@Setter
+public class DbUser {
+    private String id;
+    private String username;
+    private String password;
+    private String age;
+
+}
