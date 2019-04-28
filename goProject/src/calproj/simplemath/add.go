161f163ec46c607e950fd254cf0f2b91a166b797
ningqb <ning_qb@hisuntech.com>
2019-04-28 11:12:00 +0800

    1

--- goProject/src/calproj/simplemath/add.go
+++
@@ -1,6 +0,0 @@
-package simplemath
-
-//Add 计算两数之和
-func Add(a int, b int) int {
-	return a + b
-}
--- goProject/src/calproj/simplemath/sqrt.go
+++
@@ -1,9 +0,0 @@
-package simplemath
-
-import "math"
-
-//Sqrt 计算输入数字平方根
-func Sqrt(i int) int {
-	v := math.Sqrt(float64(i))
-	return int(v)
-}
