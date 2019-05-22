9c720a383f5ec474da8c5e0638f99f3a51667769
ningqb <ning_qb@hisuntech.com>
2019-05-22 09:27:12 +0800

    -a

--- eclipse/src/com/hisuntech/class1/example06/ForFlow001.java
+++
@@ -1,9 +0,0 @@
-package com.hisuntech.class1.example06;
-
-public class ForFlow001 {
-	public static void main (String args[]) {
-		for (int i=0; i<10; i++) {
-			System.out.println("i=" + i);
-		}
-	}
-}
